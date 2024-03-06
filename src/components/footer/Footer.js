f4e35d10e642b30c8882fb5495abb6db02914bf7
Nabil-Johny <Nabil.Johny@ibm.com>
2024-03-06 10:42:35 -0500

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,16 +10,16 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by MD Mafujul Hasan (Add your name)</h3>
+          <h3>Designed and Developed by Nabil Johny</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} MD Mafujul Hasan</h3>
+          <h3>Copyright © {year} Nabil Johny</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/MD-MAFUJUL-HASAN"
+                href="https://github.com/nabilj29"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -29,7 +29,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/md-mafujul-hasan/"
+                href="https://www.linkedin.com/in/nabil-johny/"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -39,27 +39,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/_md_mafujul_hasan_/"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <AiFillInstagram />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.facebook.com/mh.tonmoy.13"
-                style={{ color: "white" }}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaFacebookF />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="mailto:mdtonmoy13.mt@gmail.com/"
+                href="mailto:nabil.johny@gmail.com/"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
